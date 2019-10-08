de73f819b52770ba3ecef587d3fa6e018ede39aa
Michal Vagac <michal.vagac@gmail.com>
2019-10-08 15:04:41 +0200

    priprava frameworku

--- apps/backend/node_manager/node_manager.py
+++ apps/backend/node_manager/node_manager.py
@@ -2,41 +2,50 @@
 
 # PYTHONPATH musi odkazoat na absolutnu cestu k .../chodba-framework/base
 
+# tato aplikacia sa ako jedina spusta priamo z os (vzdy pri starte os); a to na kazdom uzle
+
 import sys
 import paho.mqtt.client as mqtt
+import socket
 import base_app
 
-APP_NAME = "frontend_runner"
+APP_NAME = "node_manager"
 
 # na poziadnie oznam, kde sa ma spustit: * na vsetkych, ? na lubovolnom, <n> na konkretnom
-if len(sys.argv) == 2 and sys.argv[1]=="run":
+if len(sys.argv) == 2 and sys.argv[1]=="runon":
+    # zobraz informaciu, na ktorych uzloch sa ma backend spustat
     print("*")
     sys.exit(1)
 
-NODE_NO = sys.argv[1]
+# ako parameter sa ocakava nazov uzla, kde sa backend spusta
+NODE_NAME = socket.gethostname()
+print("nazov uzla: " + NODE_NAME)
 
-class FrontendRunner(base_app.BaseApp):
+class NodeManager(base_app.BaseApp):
 
     def get_app_name(self):
         return APP_NAME
 
-    def info(self):
-        return "subscriber: run_frontend/" + NODE_NO
+    def info_pub(self):
+        return ""
+
+    def info_sub(self):
+        return "run_frontend/" + NODE_NAME
 
     def on_run_frontend_message(self, client, userdata, message):
         sprava = message.payload.decode()
         print("aaaaaaaaaaaaaaa: " + sprava)
-        #TODO spusti danu frontend appku. priklad spravy: teplomer_alert a b c => spusti danu appku nasledovne: teplomer_alert NODE_NO a b c
+        #TODO spusti danu frontend appku. priklad spravy: teplomer_alert a b c => spusti danu appku nasledovne: teplomer_alert NODE_NAME a b c
 
     def run(self):
-        self.client.message_callback_add('run_frontend/' + NODE_NO, self.on_run_frontend_message)
-        self.client.subscribe("run_frontend/" + NODE_NO)
+        self.client.message_callback_add('run_frontend/' + NODE_NAME, self.on_run_frontend_message)
+        self.client.subscribe("run_frontend/" + NODE_NAME)
         # cakaj
         app.client.loop_forever()
 
 
 if __name__ == '__main__':
-    app = FrontendRunner()
+    app = NodeManager()
     app.start()
     app.run()
 
--- apps/base/base_app.py
+++ apps/base/base_app.py
@@ -2,25 +2,35 @@
 
 import sys
 import paho.mqtt.client as mqtt
+import json
 
 BROKER_URL = "localhost"
 BROKER_PORT = 1883
 
+# zakladna trieda, ktora definuje spolocne vlastnosti pre vsetky menezovane aplikacie
+
 class BaseApp:
 
     def get_app_name(self):
+        # musi vratit nazov (identifikator) aplikacie
         raise NotImplementedError()
 
-    def info(self):
+    def info_pub(self):
+        # musi vratit retazec s informaciami o topicoch, do ktorych posiela spravy
+        raise NotImplementedError()
+
+    def info_sub(self):
+        # musi vratit retazec s informaciami o topicoch, na ktore sa prihlasuje
         raise NotImplementedError()
 
     def on_apps_message(self, client, userdata, message):
-        sprava = message.payload.decode()
-        if sprava == "quit":
+        sprava = json.loads(message.payload.decode())
+        if sprava["prikaz"] == "quit":
             self.stop()
-        elif sprava == "info":
-            self.client.publish(topic="master", payload=self.info(), qos=0, retain=False)
-        elif sprava == "status":
+        elif sprava["prikaz"] == "info":
+            info = "pub: " + self.info_pub() + "\nsub: " + self.info_sub()
+            self.client.publish(topic="master", payload=info, qos=0, retain=False)
+        elif sprava["prikaz"] == "status":
             self.client.publish(topic="master", payload="ok", qos=0, retain=False)
         else:
             print("neznama sprava: " + sprava)
