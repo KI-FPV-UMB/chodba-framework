3be727e8742a880d413cbc0f3e1c6d73af08d21a
Michal Vagac <michal.vagac@gmail.com>
2020-02-10 11:16:42 +0100

    BACKEND filtrovanie merania teploty a vlhkosti

--- backend-apps/teplota_vlhkost/teplota_vlhkost.py
+++ backend-apps/teplota_vlhkost/teplota_vlhkost.py
@@ -13,14 +13,27 @@ import json
 import base_app
 import time
 import Adafruit_DHT
+import numpy as np
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
+POCET_MERANI = 5
 
 class TeplotaVlhkost(base_app.BaseApp):
 
     def on_msg(self, msg):
         None
+
+    # vypocitaj smerodajnu odchilku z niekolkych merani a uplne odstran hodnoty, ktore prilis vybocuju (outliers)
+    def odstran_sum(self, hodnoty, std_factor = 2):
+        # https://forum.dexterindustries.com/t/solved-dht-sensor-occasionally-returning-spurious-values/2939/5
+        mean = np.mean(hodnoty)
+        standard_deviation = np.std(hodnoty)
+        if standard_deviation == 0:
+            return hodnoty
+        vysledok = [element for element in hodnoty if element > mean - std_factor * standard_deviation]
+        vysledok = [element for element in vysledok if element < mean + std_factor * standard_deviation]
+        return vysledok
 
     def run(self):
         # spracovavaj mqtt spravy
@@ -29,7 +42,15 @@ class TeplotaVlhkost(base_app.BaseApp):
         self.running = True
         while self.running:
             # zmeraj vlhkost a teplotu
-            humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+            humis = []
+            temps = []
+            for i in range(POCET_MERANI):
+                h, t = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+                humis.append(h)
+                temps.append(t)
+                time.sleep(0.1)
+            humidity = np.mean(self.odstran_sum(humis))
+            temperature = np.mean(self.odstran_sum(temps))
             # zapis do db
             msg = { "humidity": humidity, "temperature": temperature }
             self.publish_message("insert", msg, "database")
