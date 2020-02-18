872a553159c3c7d879bca2bf76c24de9b06b8958
Michal Vagac [chodba-ki] <michal.vagac@umb.sk>
2020-02-18 11:31:26 +0100

    BASE logger config

--- base/base_app.py
+++ base/base_app.py
@@ -20,12 +20,36 @@ if WEBSOCKETS:
 else:
     BROKER_PORT = 1883
 
-# zakladna trieda, ktora definuje spolocne vlastnosti pre vsetky menezovane aplikacie
+class LessThanFilter(logging.Filter):
+    def __init__(self, exclusive_maximum, name=""):
+        super(LessThanFilter, self).__init__(name)
+        self.max_level = exclusive_maximum
 
+    def filter(self, record):
+        # non-zero return means we log this message
+        return 1 if record.levelno < self.max_level else 0
+
+# zakladna trieda, ktora definuje spolocne vlastnosti pre vsetky menezovane aplikacie
 class BaseApp:
 
     def __init__(self):
-        logging.basicConfig(level=logging.DEBUG)
+        #logging.basicConfig(level=logging.DEBUG)
+        #TODO datum cas
+        logger = logging.getLogger()
+        logger.setLevel(logging.NOTSET)
+
+        formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
+
+        h1 = logging.StreamHandler(sys.stdout)
+        h1.setLevel(logging.DEBUG)
+        h1.setFormatter(formatter)
+        h1.addFilter(LessThanFilter(logging.WARNING))
+        logger.addHandler(h1)
+
+        h2 = logging.StreamHandler(sys.stderr)
+        h2.setLevel(logging.WARNING)
+        h2.setFormatter(formatter)
+        logger.addHandler(h2)
 
         # inicializuj id a node
         self.id = hex(random.getrandbits(128))[2:-1]
