cf40f729388fcf67a64d11b2309be358524d8afb
Michal Vagač <michal.vagac@gmail.com>
2022-07-14 12:43:18 +0200

    remove of temporary and unfinished applications

--- apps/demo_hra2d_p/prisera1.py
+++
@@ -1,8 +0,0 @@
-def pozicia():
-    return 1, 1
-
-def uprav(self):
-    self.x += 0.05
-    if self.x >= self.bludisko.sirka-1:
-        self.x = 0
-
--- apps/demo_hra2d_p/prisera2.py
+++
@@ -1,8 +0,0 @@
-def pozicia():
-    return 3, 1
-
-def uprav(self):
-    self.y += 0.02
-    if self.y >= self.bludisko.vyska-1:
-        self.y = 0
-
