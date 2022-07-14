aa1c5f46c0356bcc78f8ac2003da449f365982a5
Michal Vagač <michal.vagac@gmail.com>
2022-07-14 11:52:59 +0200

    quotes application

--- apps/hlasky/quotes.py
+++ apps/hlasky/quotes.py
@@ -1,53 +1,50 @@
 #!/usr/bin/python3
 
-"""skel_tkinter.py: skel aplikacia pre frontend gui v pythone"""
+"""quotes.py: quotes application"""
 __author__ = "Michal Vagac"
 __email__ = "michal.vagac@gmail.com"
 
-# PYTHONPATH musi odkazovat na absolutnu cestu k .../chodba-framework/base
+# set PYTHONPATH to project root (chodba-framework)
 
 import sys
-import os
-import paho.mqtt.client as mqtt
-import json
 import tkinter
 import tkinter.messagebox
 import random
 import subprocess
-import base_app
+from base import base_app
 
 class Hlasky(base_app.BaseApp):
 
     def run(self):
-        # spusti spracovanie mqtt
+        # start processing of mqtt messages
         self.client.loop_start()
 
-        # vyber nahodnu farbu pozadia
+        # choose random background color
         colors = ["white", "orange", "yellow", "green"]
         bgcol = random.choice(colors)
 
-        # zobraz okno
+        # show window
         self.top = tkinter.Tk()
-        self.top.wm_attributes("-type", "splash")       # bez dekoracii
+        self.top.wm_attributes("-type", "splash")       # no decorations
         self.top.wm_attributes("-fullscreen", True)
         self.top.configure(background=bgcol)
-        if self.screen_width is not None and self.screen_height is not None:
-            self.top.geometry("{0}x{1}+0+0".format(self.screen_width-3, self.screen_height-3))
+        if self.args.screen_width is not None and self.args.screen_height is not None:
+            self.top.geometry("{0}x{1}+0+0".format(self.args.screen_width-3, self.args.screen_height-3))
         else:
-            self.top.geometry("{0}x{1}+0+0".format(self.top.winfo_screenwidth()-3, self.top.winfo_screenheight()-3))    # na celu obrazovku
+            self.top.geometry("{0}x{1}+0+0".format(self.top.winfo_screenwidth()-3, self.top.winfo_screenheight()-3))    # fullscreen
 #        self.top.resizable(False, False)
 #        self.top.update_idletasks()
 #        self.top.overrideredirect(True)
 
         r = random.randrange(0, 3)
         if r < 2:
-            # daj hlasku z fortune
+            # quote from fortune
             result = subprocess.run(["/usr/games/fortune", "sk"], stdout=subprocess.PIPE)
             hlaska = result.stdout.decode("utf-8").replace("\t", "")
         else:
-            # nacitaj hlasky zo suboru
+            # quote from file
             hlasky = []
-            with open("hlasky.txt") as fp:
+            with open("quotes.txt") as fp:
                 line = fp.readline()
                 while line:
                     if not line.startswith("#"):
@@ -56,20 +53,19 @@ class Hlasky(base_app.BaseApp):
             #n = random.randint(0, len(hlasky)-1)
             hlaska = random.choice(hlasky)
 
-        # napln okno obsahom
+        # window content
         msg = tkinter.Message(self.top, text=hlaska)
         msg.config(font=('times', 70, 'italic'), bg=bgcol)
-        msg.pack(expand=True)                       # aby to bolo vertikalne v strede
+        msg.pack(expand=True)          # center vertically
 
-        # pracuj
+        # work
         self.top.mainloop()
 
     def stop(self):
-        # zastav spracovanie mqtt
+        # stop processing mqtt
         super().stop()
-        # zrus okno
+        # close window
         self.top.quit()
-        #self.top.destroy()
 
 if __name__ == '__main__':
     app = Hlasky()
