b02e25f4a7bb0d4de663d837cb8d2fce2aa4539e
Michal Vagač <michal.vagac@gmail.com>
2022-07-15 12:50:45 +0200

    restart-all.sh script

---
+++ system/scripts/usr/local/bin/restart-all.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# start on main node (with AppController)
+
+HOSTNAME=$(hostname)
+NODES=$(cat /etc/hosts | grep chodba | grep -v $HOSTNAME | sort | awk '{ print $2 }')
+
+# stop all nodes
+for NODE in $NODES
+do
+  ssh $NODE 'sudo systemctl stop chodba-node_manager.service'
+done
+
+# start all nodes
+for NODE in $NODES
+do
+  ssh $NODE 'sudo systemctl start chodba-node_manager.service'
+done
