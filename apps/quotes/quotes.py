ec106de2ee57df573dd1a9d95c14d2cc7b3420f5
Michal Vagac <michal.vagac@gmail.com>
2022-11-08 00:26:03 +0100

    Quotes App - reworked as SDL app (from tkinter)

--- apps/quotes/quotes.py
+++ apps/quotes/quotes.py
@@ -5,39 +5,96 @@ __author__ = "Michal Vagac"
 __email__ = "michal.vagac@gmail.com"
 
 # set PYTHONPATH to project root (chodba-framework)
+# apt install fortunes-cs
 
 import sys
-import tkinter
-import tkinter.messagebox
 import random
 import subprocess
+
+import sdl2
+import sdl2.ext
+import sdl2.sdlttf          # libsdl2-ttf-2.0-0
+
 from base import base_app
 
+FONT_PATH = "/usr/share/fonts/truetype/freefont/FreeSans.ttf"
+#FONT_PATH = "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf"
+#FONT_PATH = "/usr/share/fonts/truetype/ttf-liberation/LiberationSans-Regular.ttf"
+FONT_SIZE = 70
+FS_ENCODING = "utf-8"
+
 class Quotes(base_app.BaseApp):
+
+    def hex_to_rgb(self, hex):
+        return tuple(int(hex[i:i + 2], 16) for i in (0, 2, 4))
+
+    def render_text(self, s, col, x, y, w, h, border = 0, align_h='l', align_v='t'):
+        self.text = None
+        sdlCol = sdl2.SDL_Color(*self.hex_to_rgb(col[0][1:]))
+        st = s.split()
+        # text analysis and preparation
+        idx = 0
+        maxh = 0
+        lines = []
+        while (idx < len(st)):
+            # prepare text line
+            pom = ""
+            while (idx < len(st)):
+                pom += " " + st[idx]
+                if self.text is not None:
+                    sdl2.SDL_FreeSurface(self.text)
+                self.text = sdl2.sdlttf.TTF_RenderUTF8_Blended(self.font, pom[1:].encode(FS_ENCODING), sdlCol)
+                if self.text.contents.w >= w - 2*border:
+                    break
+                idx += 1
+            if self.text.contents.w >= w - 2*border:
+                # 1 word backwards
+                pom = pom.rsplit(' ', 1)[0]
+                if self.text is not None:
+                    sdl2.SDL_FreeSurface(self.text)
+                self.text = sdl2.sdlttf.TTF_RenderUTF8_Blended(self.font, pom[1:].encode(FS_ENCODING), sdlCol)
+            lines.append(pom[1:])
+            # next line
+            maxh += self.text.contents.h
+
+        if align_v == 't':
+            yt = y + border
+        if align_v == 'm':
+            yt = y + h//2 - maxh//2
+        if align_v == 'b':
+            yt = y + h - border - maxh
+        for l in lines:
+            if self.text is not None:
+                sdl2.SDL_FreeSurface(self.text)
+            self.text = sdl2.sdlttf.TTF_RenderUTF8_Blended(self.font, l.encode(FS_ENCODING), sdlCol)
+            r = sdl2.SDL_Rect()
+            r.y = yt
+            if align_h == 'l':
+                r.x = x + border
+            if align_h == 'r':
+                r.x = x + w - border - self.text.contents.w
+            if align_h == 'c':
+                r.x = x + w//2 - self.text.contents.w//2
+            r.w, r.h = self.text.contents.w, self.text.contents.h
+            sdl2.SDL_BlitSurface(self.text, None, self.windowsurface, r)
+            # next line
+            yt += self.text.contents.h
+
+        if self.text is not None:
+            sdl2.SDL_FreeSurface(self.text)
+        self.text = None
 
     def run(self):
         # choose random background color
         colors = [
-            ["#000", "#81b29a"], ["#000", "#f2cc8f"],
-            ["#fff", "#3d405b"], ["#000", "#81b29a"],
-            ["#fff", "#e07a5f"], ["#fff", "#3d405b"],
-            ["#000", "#f4f1de"], ["#fff", "#e07a5f"],
+            ["#000000", "#81b29a"], ["#000000", "#f2cc8f"],
+            ["#ffffff", "#3d405b"], ["#000000", "#81b29a"],
+            ["#ffffff", "#e07a5f"], ["#ffffff", "#3d405b"],
+            ["#000000", "#f4f1de"], ["#ffffff", "#e07a5f"],
         ]
         col = random.choice(colors)
 
-        # show window
-        self.top = tkinter.Tk()
-        self.top.wm_attributes("-type", "splash")       # no decorations
-        self.top.wm_attributes("-fullscreen", True)
-        self.top.configure(background=col[1])
-        if self.args.screen_width is not None and self.args.screen_height is not None:
-            self.top.geometry("{0}x{1}+0+0".format(self.args.screen_width-3, self.args.screen_height-3))
-        else:
-            self.top.geometry("{0}x{1}+0+0".format(self.top.winfo_screenwidth()-3, self.top.winfo_screenheight()-3))    # fullscreen
-#        self.top.resizable(False, False)
-#        self.top.update_idletasks()
-#        self.top.overrideredirect(True)
-
+        # prepare text
         r = random.randrange(0, 3)
         if r < 2:
             # quote from fortune
@@ -55,22 +112,47 @@ class Quotes(base_app.BaseApp):
             #n = random.randint(0, len(quotes)-1)
             quote = random.choice(quotes)
 
-        # window content
-        msg = tkinter.Message(self.top, text=quote)
-        msg.config(font=('times', 70, 'italic'), fg=col[0], bg=col[1])
-        msg.pack(expand=True)          # center vertically
+        # sdl init
+        sdl2.ext.init()
+        if sdl2.sdlttf.TTF_Init() < 0:
+            sys.exit(1)
+
+        # create and show window (full screen)
+        flags = sdl2.SDL_WINDOW_SHOWN | sdl2.SDL_WINDOW_FULLSCREEN_DESKTOP | sdl2.SDL_WINDOW_BORDERLESS
+        self.window = sdl2.ext.Window("Quotes", size=(640, 480), position=(0, 0), flags=flags)
+        self.window.show()
+        window_w, window_h = self.window.size
+        self.windowsurface = self.window.get_surface()
+
+        # clear window and render text
+        sdl2.ext.fill(self.windowsurface, sdl2.ext.Color(*self.hex_to_rgb(col[1][1:])))
+        self.font = sdl2.sdlttf.TTF_OpenFont(FONT_PATH.encode("ascii"), FONT_SIZE)
+        self.render_text(quote, col, 0, 0, window_w, window_h, 15, 'c', 'm')
+        self.window.refresh()
 
         # start processing of mqtt messages
         super().run()
 
-        # work
-        self.top.mainloop()
+        # event loop
+        self.running = True
+        event = sdl2.SDL_Event()
+        while self.running:
+            events = sdl2.ext.get_events()
+            for event in events:
+                # process events
+                if event.type == sdl2.SDL_QUIT:             # event: quit
+                    self.running = False
+                    break
+            sdl2.SDL_Delay(500)                             # in ms
+
+        # release resources
+        sdl2.ext.quit()
 
     def stop(self):
         # stop processing mqtt
         super().stop()
-        # close window
-        self.top.destroy()  # quit()
+        # stop processing SDL events
+        self.running = False
 
 if __name__ == '__main__':
     app = Quotes()
