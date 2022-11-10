c6f61c68116212201fd8e09b5c3649f3e9517f04
Michal Vagač <michal.vagac@gmail.com>
2022-11-10 16:20:26 +0100

    SKEL SDL C event loop

--- skel/skel_sdl-c/skel_sdl-c.c
+++ skel/skel_sdl-c/skel_sdl-c.c
@@ -6,14 +6,27 @@
 
 int main(int argc, char *argv[])
 {
-
-	// returns zero on success else non-zero
+	// initialize
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		printf("error initializing SDL: %s\n", SDL_GetError());
 	}
-	SDL_Window* win = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
-	while (1)
-		;
+	SDL_Window* win = SDL_CreateWindow("Chodba", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
+
+	// event loop
+	int running = 1;
+	while (running) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_QUIT:
+					running = 0;
+					break;
+			}
+		}
+	}
+
+	// release
+	SDL_Quit();
 
 	return 0;
 }
